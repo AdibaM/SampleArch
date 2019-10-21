8e245754d89ca651848ced28fb1f4fb5b2ea7bc7
Adiba Maduegbuna <adiba@wearemoody.com>
2019-10-21 12:20:00 +0100

    Create custom ArtistCardView in core module & use for feed items

---
+++ Code/core/src/main/java/com/adibam/core/ui/ArtistCardView.kt
@@ -0,0 +1,26 @@
+package com.adibam.core.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.adibam.core.R
+import com.adibam.core.entities.Artist
+import kotlinx.android.synthetic.main.artist_card_view.view.*
+
+/**
+ * In this sample app, artist cards can be displayed across the app
+ * As a result its a good candidate for the core module
+ */
+class ArtistCardView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.artist_card_view, this, true)
+    }
+
+    fun bindView(artist: Artist) {
+        artist_name.text = artist.name
+    }
+}
--- Code/feed/src/main/java/com/adibam/feed/ui/FeedViewHolder.kt
+++ Code/feed/src/main/java/com/adibam/feed/ui/FeedViewHolder.kt
@@ -1,15 +1,15 @@
 package com.adibam.feed.ui
 
 import android.view.View
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.adibam.core.entities.Artist
+import com.adibam.core.ui.ArtistCardView
 
 class FeedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(artist: Artist) {
-        val view = itemView as TextView
-        view.text = artist.name
+        val artistView = itemView as ArtistCardView
+        artistView.bindView(artist)
     }
 
 }
